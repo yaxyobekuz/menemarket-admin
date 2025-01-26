11864f0b5174534857e2c4189e4f1a1975e05b26
yaxyobekb <pubgn9642@gmail.com>
2025-01-26 23:27:47 +0500

    Add main layout tabs data with localized names

---
+++ src/data/mainLayoutTabs.js
@@ -0,0 +1,54 @@
+export default [
+  {
+    path: "/",
+    name: "Bosh sahifa",
+  },
+  {
+    path: "/products",
+    name: "Mahsulotlar",
+  },
+  {
+    path: "/streams",
+    name: "Oqimlar",
+  },
+  {
+    path: "/orders",
+    name: "Buyurtmalar",
+  },
+  {
+    path: "/users",
+    name: "Foydalanuvchilar",
+  },
+  {
+    path: "/comments",
+    name: "Izohlar",
+  },
+  {
+    path: "/payments",
+    name: "To'lovlar",
+  },
+  {
+    path: "/contests",
+    name: "Konkurslar",
+  },
+  {
+    path: "/donation-box",
+    name: "Hayriya qutisi",
+  },
+  {
+    path: "/news",
+    name: "Yangiliklar",
+  },
+  {
+    path: "/blogs",
+    name: "Bloglar",
+  },
+  {
+    path: "/proofile",
+    name: "Profil",
+  },
+  {
+    path: "/settings",
+    name: "Sozlamalar",
+  },
+];
