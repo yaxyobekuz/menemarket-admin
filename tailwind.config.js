f1cedfc69daebd8e54e55d4a358635bc76a497c4
yaxyobekb <pubgn9642@gmail.com>
2025-01-26 23:28:15 +0500

    Refactor Tailwind CSS configuration to enhance theme customization and add maxWidth utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		colors: {
-  			primary: {
-  				default: '#0085FF',
-  				strong: '#1A3EDD'
-  			},
-  			neutral: {
-  				dark: '#333333'
-  			},
-  			gray: {
-  				light: '#f5f5f5',
-  				medium: '#d1d5db'
-  			}
-  		},
-  		fontFamily: {
-  			Inter: [
-  				'Inter',
-  				'sans-serif'
-  			]
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	},
-  	screens: {
-  		'2xl': '1280px',
-  		xl: '1140px',
-  		lg: '1024px',
-  		md: '768px',
-  		sm: '640px',
-  		xs: '425px'
-  	}
+    extend: {
+      maxWidth: {
+        base: "1920px",
+      },
+      colors: {
+        primary: {
+          default: "#0085FF",
+          strong: "#1A3EDD",
+        },
+        neutral: {
+          dark: "#333333",
+        },
+        gray: {
+          light: "#f5f5f5",
+          medium: "#d1d5db",
+        },
+      },
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+    screens: {
+      xl: "1280px",
+      lg: "1024px",
+      md: "768px",
+      sm: "640px",
+      xs: "425px",
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
