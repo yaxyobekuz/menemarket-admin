6fdd5661225ec4fcbe193d231518444fcca9e476
yaxyobekb <pubgn9642@gmail.com>
2025-01-26 23:30:51 +0500

    Add Header component for site branding and navigation

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+
+// Components
+import Icon from "./Icon";
+
+// Images
+import logoIcon from "../assets/images/icons/logo.svg";
+
+const Header = () => {
+  return (
+    <div className="fixed inset-x-0 top-0 z-10 w-full h-14 bg-white border-b border-neutral-200">
+      <div className="flex w-full h-full px-5">
+        <Link to="/" className="flex items-center h-full">
+          <img
+            width={75}
+            height={40}
+            src={logoIcon}
+            alt="Mene Market Logo"
+            className="w-[75px] h-10"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
