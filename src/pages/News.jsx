d421a55f666f6b9ce2df71113fe2d3e9c0256ec9
yaxyobekb <pubgn9642@gmail.com>
2025-01-29 20:56:39 +0500

    Add News page to display and manage news items with delete functionality

---
+++ src/pages/News.jsx
@@ -0,0 +1,129 @@
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+
+// Toaster (For notification)
+import { notification } from "@/notification";
+
+// Components
+import Icon from "@/components/Icon";
+import NewsItem from "@/components/NewsItem";
+import DotsLoader from "@/components/DotsLoader";
+
+// Services
+import newsService from "@/api/services/newsService";
+
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+import { updateNews } from "@/store/features/newsSlice";
+
+// Images
+import reloadIcon from "@/assets/images/icons/reload.svg";
+
+const News = () => {
+  const dispatch = useDispatch();
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const allNews = useSelector((state) => state.news.data);
+  const [filteredNews, setFilteredNews] = useState(allNews || []);
+
+  const handleDeleteNews = (id) => {
+    notification.promise(
+      newsService.deleteNews(id).then(() => {
+        setFilteredNews((news) => news.filter(({ _id }) => _id !== id));
+      }),
+      {
+        loading: "Yangilik o'chirilmoqda...",
+        success: "Yangilik muvaffaqiyatli o'chirildi!",
+        error: "Yangilikni o'chirishda xatolik yuz berdi!",
+      }
+    );
+  };
+
+  const loadNews = () => {
+    setHasError(false);
+    setIsLoading(true);
+
+    newsService
+      .getNews()
+      .then((news) => {
+        setFilteredNews(news);
+        dispatch(updateNews(news));
+      })
+      .catch(() => setHasError(true))
+      .finally(() => setIsLoading(false));
+  };
+
+  useEffect(() => {
+    if (allNews?.length === 0) loadNews();
+    else setTimeout(() => setIsLoading(false), 300);
+  }, []);
+
+  return (
+    <div className="container py-6 space-y-7">
+      <h1>Yangiliklar</h1>
+
+      {/* Nav tabs */}
+      <nav className="products-layout-tabs">
+        <ul className="flex gap-1 max-w-max bg-white p-1 rounded-xl">
+          {/* Main */}
+          <li>
+            <NavLink
+              end
+              to="/news"
+              className="inline-block py-2 px-5 rounded-lg text-[17px] text-neutral-500 transition-colors duration-200 hover:bg-gray-light/50"
+            >
+              Asosiy
+            </NavLink>
+          </li>
+
+          {/* Product */}
+          <li>
+            <NavLink
+              to="/news/new"
+              className="inline-block py-2 px-5 rounded-lg text-[17px] text-neutral-500 transition-colors duration-200 hover:bg-gray-light/50"
+            >
+              Yaratish
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      {/* News */}
+      {!isLoading && !hasError && filteredNews?.length >= 0 && (
+        <ul className="grid grid-cols-6 gap-5">
+          {filteredNews.map((news) => (
+            <NewsItem
+              data={news}
+              key={news?._id}
+              deleteNews={handleDeleteNews}
+            />
+          ))}
+        </ul>
+      )}
+
+      {/* Loading animation */}
+      {isLoading && !hasError && (
+        <DotsLoader
+          color="#0085FF"
+          className="flex justify-center fixed top-1/2 inset-x-0 w-full"
+        />
+      )}
+
+      {/* Reload button */}
+      {hasError && !isLoading && (
+        <div className="flex justify-center fixed top-[calc(50%-20px)] inset-x-0">
+          <button
+            title="Reload"
+            onClick={loadNews}
+            aria-label="Reload"
+            className="flex items-center justify-center size-10"
+          >
+            <Icon src={reloadIcon} alt="Reload icon" />
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default News;
