08560a9707cce778a3761c6fc6c548bb1ea6f0f8
yaxyobekb <pubgn9642@gmail.com>
2025-01-31 21:50:22 +0500

    Add token validation in MainLayout to redirect unauthenticated users to login

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -7,11 +7,9 @@ import MainLayoutTabs from "@/components/MainLayoutTabs";
 
 const MainLayout = () => {
   const location = useLocation();
-  const pathArr = location.pathname.split("/").filter((_) => _);
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location.pathname]);
+  const token = localStorage.getItem("token");
+  if (!token) return (window.location.pathname = "/login");
+  useEffect(() => window.scrollTo(0, 0), [location.pathname]);
 
   return (
     <div className="flex flex-col min-h-screen pb-16 md:pb-0">
