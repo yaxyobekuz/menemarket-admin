a2dd010ab65a10f46cb2e84a9f973aecc6f56d65
yaxyobekb <pubgn9642@gmail.com>
2025-01-31 11:21:13 +0500

    Refactor OrderItem component to use StickyCell and TruncatedCell for improved layout and styling

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -4,6 +4,10 @@ import { Link } from "react-router-dom";
 // Data
 import addresses from "@/data/addresses";
 
+// Components
+import StickyCell from "./StickyCell";
+import TruncatedCell from "./TruncatedCell";
+
 // Utils
 import { extractNumbers, formatDate, getPercentageBgColor } from "@/utils";
 
@@ -22,54 +26,40 @@ const OrderItem = ({ data = {}, index = 0, isScrolled }) => {
     addresses.find(({ value }) => value == address)?.name || "Mavjud emas";
 
   return (
-    <tr key={index} className="group h-12 bg-neutral-50 even:bg-white">
+    <tr className="group h-12 bg-neutral-50 even:bg-white">
       {/* Index */}
-      <td
-        className={`${
-          isScrolled ? "custom-active-border-r" : null
-        } sticky left-0 inset-y-0 bg-neutral-50 text-center transition-colors duration-200 group-even:bg-white`}
-      >
-        {index}
-      </td>
+      <StickyCell children={index} isActive={isScrolled} />
 
       {/* User */}
-      <td className="text-center">
-        <span className="line-clamp-2">{firstName}</span>
-      </td>
+      <TruncatedCell trunc="line-clamp-2">{firstName}</TruncatedCell>
 
       {/* Address */}
-      <td className="text-center">
-        <address className="not-italic">{formattedAddress}</address>
+      <td>
+        <address>{formattedAddress}</address>
       </td>
 
       {/* Phone */}
-      <td className="text-center">
-        <a
-          href={`tel:+${extractNumbers(tel)}`}
-          className="transition-colors duration-200 hover:text-primary-default"
-        >
+      <td>
+        <a href={`tel:+${extractNumbers(tel)}`} className="primary-link">
           {tel}
         </a>
       </td>
 
       {/* Status */}
-      <td className="text-center">{status}</td>
+      <td>{status}</td>
 
-      {/* Product */}
-      <td className="text-center">
-        <Link
-          to={`/products/product/${productId}`}
-          className="transition-colors duration-200 hover:text-primary-default"
-        >
+      {/* Product ID */}
+      <td>
+        <Link to={`/products/product/${productId}`} className="primary-link">
           {productId}
         </Link>
       </td>
 
       {/* Price */}
-      <td className="text-center">{price?.toLocaleString()} so'm</td>
+      <td>{price?.toLocaleString()} so'm</td>
 
       {/* Date */}
-      <td className="text-center">{formatDate(timestamp)}</td>
+      <td>{formatDate(timestamp)}</td>
 
       <td>
         <div
