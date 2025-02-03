3ebf10db91ac5950e4d86113ef611735038ea5d7
yaxyobekb <pubgn9642@gmail.com>
2025-02-03 20:58:27 +0500

    Add Repair component with Lottie animation to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+// Components
+import Repair from "@/components/Repair";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2">
+      <Repair />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
