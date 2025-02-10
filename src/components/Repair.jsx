af6d414ec81a13ad824bc18a2ec23b4addd528e6
yaxyobekb <pubgn9642@gmail.com>
2025-02-10 22:07:53 +0500

    Add PageMessage component and refactor Repair component to use it

--- src/components/Repair.jsx
+++ src/components/Repair.jsx
@@ -1,23 +1,7 @@
-import Lottie from "lottie-react";
+import PageMessage from "./PageMessage";
 
-// Stickers
-import gearSticker from "../assets/stickers/gear.json";
-
-const Repair = ({ title = "Sahifa ta'mirlanmoqda...", className = "" }) => {
-  return (
-    <div className={`flex flex-col items-center gap-5 max-w-max ${className}`}>
-      {/* Sticker */}
-      <Lottie
-        animationData={gearSticker}
-        className="size-28 xs:size-32 sm:size-36 md:size-40"
-      />
-
-      {/* Title */}
-      <span className="text-nowrap text-lg font-medium text-center xs:text-xl sm:font-semibold sm:text-2xl">
-        {title}
-      </span>
-    </div>
-  );
+const Repair = ({ title = "Sahifa ta'mirlanmoqda...", className }) => {
+  return <PageMessage title={title} className={className} />;
 };
 
 export default Repair;
