017a1f5e3fd100f1c7ec6a3cd23f97326f2f8bbb
yaxyobekb <pubgn9642@gmail.com>
2025-02-15 12:58:41 +0500

    Refactor CreateWorker component to streamline user creation process and add role selection

--- src/pages/CreateWorker.jsx
+++ src/pages/CreateWorker.jsx
@@ -16,11 +16,9 @@ import LoadingText from "@/components/LoadingText";
 import FormInputWrapper from "@/components/FormInputWrapper";
 
 const Users = () => {
-  const [code, setCode] = useState("");
-  const [userId, setUserId] = useState(null);
-  const [formData, setFormData] = useState({});
-  const [loginData, setLoginData] = useState({});
+  const [user, setUser] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({ status: "admin" });
 
   // Update form data
   const handleInputChange = useCallback((field, value) => {
@@ -33,64 +31,26 @@ const Users = () => {
   const handleWorkerCreate = (e) => {
     e.preventDefault();
 
-    const { name, email, password } = formData || {};
+    const { name, email, password, status } = formData || {};
+
     const isValidFormData =
-      name?.length > 1 && email?.length > 3 && password?.length > 3;
+      name?.length && email?.length && password?.length && status?.length;
 
     if (isLoading || !isValidFormData) {
       return notification.error("Ma'lumotlar to'g'ri kiritilmadi");
     }
 
     setIsLoading(true);
-    setLoginData(formData);
 
     usersService
       .createWorker(formData)
-      .then(({ data, status }) => {
-        const { userid: userId } = data || {};
-
-        if (status !== "KUTILMOQDA") {
-          return notification.error("Nimadir xato ketdi");
-        }
-
-        setUserId(userId);
+      .then(({ name, email, status, _id: id }) => {
+        if (name && email && status) {
+          setUser({ name, email, password, status, id });
+          notification.success("Yangi ishchi yaratildi");
+        } else notification.error("Nimadir xato ketdi");
       })
       .catch(() => notification.error("Nimadir xato ketdi"))
-      .finally(() => setIsLoading(false));
-  };
-
-  const handelVerifyOtp = (e) => {
-    e.preventDefault();
-
-    if (isLoading || !userId) {
-      return notification.error("Hali akkaunt yaratilinmadi");
-    }
-
-    if (extractNumbers(code)?.length !== 4) {
-      return notification.error("Kod noto'g'ri kiritildi");
-    }
-
-    setIsLoading(true);
-
-    const formData = {
-      userid: userId,
-      code: extractNumbers(code),
-    };
-
-    authService
-      .verifyOtp(formData)
-      .then(({ status, message }) => {
-        if (status === "TEKSHIRILDI") {
-          const { name: firstName, email, password } = loginData || {};
-          const message = `Akkaunt muvaffaqiyatli tasdiqlandi\n\nIsm: ${firstName}\nE-pochta: ${email}\nParol: ${password}`;
-          alert(message);
-        } else {
-          notification.error(message || "Noma'lum xatolik yuz berdi");
-        }
-      })
-      .catch(() => {
-        notification.error("Kodni tekshirishda noma'lum xatolik yuz berdi");
-      })
       .finally(() => setIsLoading(false));
   };
 
@@ -102,85 +62,111 @@ const Users = () => {
       <Tabs name="users" />
 
       {/* Create */}
-      {!userId && (
-        <section className="bg-white space-y-3.5 p-8 pt-6 rounded-xl">
-          <h2>Yaratish</h2>
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
+        <form
+          onSubmit={handleWorkerCreate}
+          className="bg-white space-y-5 p-8 pt-6 rounded-xl"
+        >
+          <div className="flex items-end gap-5 w-full">
+            <FormInputWrapper
+              required
+              label="Ism *"
+              name="firstName"
+              disabled={isLoading}
+              placeholder="Admin #1"
+              className="gray-input"
+              onChange={(value) => handleInputChange("name", value)}
+            />
 
-          {/* Form */}
-          <form className="space-y-5" onSubmit={handleWorkerCreate}>
-            <div className="flex gap-5 w-full">
-              <FormInputWrapper
-                required
-                type="email"
-                name="email"
-                label="E-pochta *"
-                disabled={isLoading}
-                className="gray-input"
-                placeholder="Misol@gmail.com"
-                onChange={(value) => handleInputChange("email", value)}
-              />
+            <div className="group flex flex-col items-center justify-center gap-2 relative overflow-hidden w-full rounded-b-lg">
+              <div className="w-full">
+                <label htmlFor="role" className="pl-1.5">
+                  Roli *
+                </label>
+              </div>
 
-              <FormInputWrapper
-                required
-                name="password"
-                type="password"
-                label="Parol *"
+              {/* Select */}
+              <select
+                name="role"
+                id="role"
                 disabled={isLoading}
-                className="gray-input"
-                placeholder="Kamida 8ta belgi"
-                onChange={(value) => handleInputChange("password", value)}
-              />
+                className="h-11 px-3.5 bg-gray-light"
+                onChange={(e) => handleInputChange("status", e.target.value)}
+              >
+                <option value="admin">Admin</option>
+                <option value="courier">Yetkazuvchi</option>
+                <option value="operator">Operator</option>
+              </select>
             </div>
+          </div>
 
-            {/* Bottom */}
-            <div className="flex items-end gap-5 w-full">
-              <FormInputWrapper
-                required
-                label="Ism *"
-                name="firstName"
-                disabled={isLoading}
-                placeholder="Admin #1"
-                className="gray-input"
-                onChange={(value) => handleInputChange("name", value)}
-              />
+          <div className="flex gap-5 w-full">
+            <FormInputWrapper
+              required
+              type="email"
+              name="email"
+              label="E-pochta *"
+              disabled={isLoading}
+              className="gray-input"
+              placeholder="Misol@gmail.com"
+              onChange={(value) => handleInputChange("email", value)}
+            />
 
-              {/* Submit button */}
-              <button disabled={isLoading} className="btn-primary w-full h-11">
-                <LoadingText text="Yaratish" loader={isLoading} />
-              </button>
-            </div>
-          </form>
-        </section>
-      )}
+            <FormInputWrapper
+              required
+              name="password"
+              type="password"
+              label="Parol *"
+              disabled={isLoading}
+              className="gray-input"
+              placeholder="Kamida 8ta belgi"
+              onChange={(value) => handleInputChange("password", value)}
+            />
+          </div>
 
-      {/* Verify OTP */}
-      {userId && (
-        <section className="bg-white space-y-3.5 p-8 pt-6 rounded-xl">
-          <h2>Tasdiqlash</h2>
+          {/* Submit button */}
+          <button disabled={isLoading} className="btn-primary w-full h-11">
+            <LoadingText text="Yaratish" loader={isLoading} />
+          </button>
+        </form>
 
-          {/* Form */}
-          <form onSubmit={handelVerifyOtp} className="space-y-5">
-            {/* Bottom */}
-            <div className="flex items-end gap-5 w-full">
-              <FormInputWrapper
-                required
-                name="otp"
-                type="otp"
-                label="Kod *"
-                disabled={isLoading}
-                placeholder="_ _ _ _"
-                className="gray-input"
-                onChange={(value) => setCode(value)}
-              />
+        {/* User data */}
+        <div className="flex items-center bg-white space-y-3.5 p-8 pt-6 rounded-xl">
+          <ul className="space-y-5">
+            {/* ID */}
+            <li className="flex items-center gap-3">
+              <h3 className="font-medium">ID: </h3>
+              <p className="text-neutral-500">{user?.id || "Kutilmoqda"}</p>
+            </li>
 
-              {/* Submit button */}
-              <button disabled={isLoading} className="btn-primary w-full h-11">
-                <LoadingText text="Tasdiqlash" loader={isLoading} />
-              </button>
-            </div>
-          </form>
-        </section>
-      )}
+            {/* First name */}
+            <li className="flex items-center gap-3">
+              <h3 className="font-medium">Ism: </h3>
+              <p className="text-neutral-500">{user?.name || "Kutilmoqda"}</p>
+            </li>
+
+            {/* Role */}
+            <li className="flex items-center gap-3">
+              <h3 className="font-medium">Rol: </h3>
+              <p className="text-neutral-500">{user?.status || "Kutilmoqda"}</p>
+            </li>
+
+            {/* Email */}
+            <li className="flex items-center gap-3">
+              <h3 className="font-medium">E-pochta: </h3>
+              <p className="text-neutral-500">{user?.email || "Kutilmoqda"}</p>
+            </li>
+
+            {/* Password */}
+            <li className="flex items-center gap-3">
+              <h3 className="font-medium">Parol: </h3>
+              <p className="text-neutral-500">
+                {user?.password || "Kutilmoqda"}
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
